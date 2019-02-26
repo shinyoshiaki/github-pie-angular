8685acce811b0574075cec879e741b480ac9586b
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-26 10:46:27 +0900

    rename

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'd3-angular'`, () => {
+  it(`should have as title 'github-pie-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('d3-angular');
+    expect(app.title).toEqual('github-pie-angular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to d3-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to github-pie-angular!');
   });
 });
