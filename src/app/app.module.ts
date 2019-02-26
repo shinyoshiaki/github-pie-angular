a19e0ef42f9af6d36eee9ed37175e7d3713c849d
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-26 10:35:23 +0900

    pwa build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { InputNameComponent } from "./components/input-name/input-name.component
 import { FormsModule } from "@angular/forms";
 import { PiechartComponent } from "./components/piechart/piechart.component";
 import { LanguageListComponent } from "./components/language-list/language-list.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,7 @@ import { LanguageListComponent } from "./components/language-list/language-list.
     PiechartComponent,
     LanguageListComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
